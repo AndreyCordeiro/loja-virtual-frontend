c81f96fbb1a3cd8544cdcf943c60c0b939be2f2f
Andrey <andreybr2002@hotmail.com>
2022-11-15 14:49:42 -0300

    Implementado crud de estado

--- frontend/src/service/cadastros/EstadoService.js
+++ frontend/src/service/cadastros/EstadoService.js
@@ -3,8 +3,30 @@ import { ServiceBase } from './ServiceBase';
 
 export class EstadoService extends ServiceBase {
 
-constructor(){
-    super("estado");
-}
+    constructor() {
+        super("estado");
+    }
 
+    url = 'http://localhost:8080/api/estado';
+
+
+    listarTodos() {
+        return axios.get(this.url);
+    }
+
+    buscarId(id) {
+        return axios.get(this.url + id);
+    }
+
+    inserir(objeto) {
+        return axios.post(this.url + '/cadastrar', objeto);
+    }
+
+    alterar(objeto) {
+        return axios.put(this.url + '/atualizar/' + objeto.id, objeto);
+    }
+
+    excluir(id) {
+        return axios.delete(this.url + '/deletar/' + id);
+    }
 }
